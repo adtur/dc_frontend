c27a05cc85090c2620a2372b4c478601306aeef6
akxs14 <angelos.kapsimanis@madvertise.com>
2013-12-15 23:39:42 +0100

    Added controller stuff in angular.html

--- app/scripts/angular_app.js
+++ app/scripts/angular_app.js
@@ -9,3 +9,15 @@ function MyCtrl($scope) {
     return $scope.visible === true;
   };
 }
+
+function MyCtrl2($scope) {
+  $scope.value = 1;
+
+  $scope.incrementValue = function(value) {
+    $scope.value += 1;
+  };
+
+  $scope.getIncrementedValue = function() {
+    return $scope.value + 1;
+  }
+}
